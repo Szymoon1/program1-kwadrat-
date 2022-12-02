098d39eb774a178e9dc44d64fad7d9eeee32f457
szymo <szymo@LAPTOP-J9A9NN0B>
2022-12-02 11:53:47 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj szerokość boku x: ");
+            string xs = Console.ReadLine();
+
+            Console.Write("Podaj długość boku y: ");
+            string ys = Console.ReadLine();
+
+            int x = int.Parse(xs);
+            int y = int.Parse(ys);
+
+
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
